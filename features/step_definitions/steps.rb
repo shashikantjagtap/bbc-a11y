f93d19da1355b06f0cb254b92cb328456866340d
Matt Wynne <matt@mattwynne.net>
2015-10-01 11:47:41 +0100

    Automate some of the steps

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -11,3 +11,25 @@ Given(/^one test fails$/) do
   WebServer.ensure_running_on(54321)
   step "I run `a11y http://localhost:54321/missing_header.html`"
 end
+
+require 'aruba/cucumber'
+Given(/^a page with this HTML:$/) do |body|
+  write_file 'test.html', <<-BODY
+<!DOCTYPE html>
+<html>
+  <body>
+    #{body}
+  </body>
+</html>
+BODY
+end
+
+When(/^I run the tool$/) do
+  step "I run `a11y test.html`"
+end
+
+Then(/^I see the warnings:$/) do |warnings|
+  warnings.raw.flatten.each do |warning|
+    assert_partial_output all_output, warning
+  end
+end
